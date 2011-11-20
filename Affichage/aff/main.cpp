bb2fbf81885e6b8e2b532dc936846852c03bd42f
Poulet Thomas <dark.flames0@gmail.com>
2011-11-20 21:35:55 +0100

    module affichage

---
+++ Affichage/aff/main.cpp
@@ -0,0 +1,342 @@
+#include <iostream>
+#include <SDL.h>
+#include <time.h>
+
+bool init_SDL();
+bool create_win_SDL();
+void refresh_SDL();
+void draw_damier();
+void draw_pions_plat();
+void draw_pion(int x, int y, int color);
+void event_SDL(SDL_Event event);
+void random_plateau();
+void tour();
+
+using namespace std;
+
+SDL_Surface *screen;
+SDL_Surface *recta, *fond;
+SDL_Surface *blanc, *noir;
+SDL_Surface *pion_j, *pion_n, *pion_r;
+SDL_Rect position;
+
+int pion[8][8];
+int continuer;
+
+int x;
+int y;
+
+bool clic;
+int player,oldplayer;
+
+int main ( int argc, char** argv )
+{
+    bool depla=false;
+    bool first[2] = {true,true};
+    player=1;
+    int color=0;
+
+    continuer=1;
+    clic=false;
+
+    SDL_Event event;
+
+    /*pion[7][2]=2;
+    pion[5][3]=1;
+    pion[4][5]=2;
+    pion[6][7]=3;*/
+
+    // initialisation SDL
+    if(init_SDL())
+    {
+        return 0;
+    }
+
+    // creation fenetre
+    if(create_win_SDL())
+    {
+        return 0;
+    }
+
+    fond = SDL_LoadBMP("image/1/fond.bmp");
+    recta = SDL_LoadBMP("image/1/cadre.bmp");
+    blanc = SDL_LoadBMP("image/1/blanc.bmp");
+    noir = SDL_LoadBMP("image/1/noir.bmp");
+    pion_j = SDL_LoadBMP("image/1/pion_j.bmp");
+    SDL_SetColorKey(pion_j, SDL_SRCCOLORKEY, SDL_MapRGB(pion_j->format, 255, 255, 255));
+    pion_n = SDL_LoadBMP("image/1/pion_n.bmp");
+    SDL_SetColorKey(pion_n, SDL_SRCCOLORKEY, SDL_MapRGB(pion_n->format, 24, 255, 0));
+    pion_r = SDL_LoadBMP("image/1/pion_r.bmp");
+    SDL_SetColorKey(pion_r, SDL_SRCCOLORKEY, SDL_MapRGB(pion_r->format, 24, 255, 0));
+
+
+    random_plateau();
+
+    //boucle jeu
+    while(continuer)
+    {
+        oldplayer=player;
+        while(player==oldplayer && continuer)
+        {
+            event_SDL(event);
+
+            /*if(first[player+1] && clic)
+            {
+                x=(x-50)/62;
+                y=(y-50)/62;
+                if(pion[x][y]==1)
+                {
+                    pion[x][y]=0;
+                    first[player+1]=false;
+                    tour();
+                }
+                clic=false;
+            }*/
+
+
+            SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, 238, 143, 15));
+            position.x=0;
+            position.y=0;
+            SDL_BlitSurface(fond,NULL,screen,&position);
+
+            position.x=25;
+            position.y=25;
+            //SDL_FillRect(recta,NULL,SDL_MapRGB(recta->format,192,121,41));
+            SDL_BlitSurface(recta,NULL,screen,&position);
+
+            draw_damier();
+            draw_pions_plat();
+
+            if(depla)
+            {
+                draw_pion(x-21,y-21,color);
+            }
+
+            if(clic && !depla)
+            {
+                depla=true;
+                x=(x-50)/62;
+                y=(y-50)/62;
+                color=pion[x][y];
+                pion[x][y]=0;
+                draw_pion(x-21,y-21,color);
+            }
+
+            if(depla && !clic)
+            {
+                x=(x-50)/62;
+                y=(y-50)/62;
+
+                pion[x][y]=color;
+
+                depla=false;
+                tour();
+            }
+
+            refresh_SDL();
+        }
+    }
+
+    SDL_FreeSurface(recta);
+    SDL_FreeSurface(noir);
+    SDL_FreeSurface(blanc);
+    SDL_Quit();
+    return 0;
+}
+
+bool init_SDL()
+{
+    if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        cout << "Impossible d'initialiser la SDL : " << SDL_GetError() << endl;
+        return 1;
+    }
+    return 0;
+}
+
+bool create_win_SDL()
+{
+    screen = SDL_SetVideoMode(600, 600, 16,SDL_HWSURFACE|SDL_DOUBLEBUF);
+    if (!screen)
+    {
+        cout << "Impossible d'initialiser la fenetre SDL :" << SDL_GetError() << endl;
+        return 1;
+    }
+    return 0;
+}
+
+void refresh_SDL()
+{
+    SDL_Flip(screen);
+    SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, 0, 0, 0));
+}
+
+void draw_damier()
+{
+    int altern_color=1;
+
+        for(int j=0;j<8;j++)
+        {
+            for(int i=0;i<8;i++)
+            {
+                position.x=50+(62*i);
+                position.y=50+(62*j);
+                if(altern_color%2)
+                {
+                    //SDL_FillRect(blanc,NULL,SDL_MapRGB(recta->format,255,255,255));
+                    SDL_BlitSurface(blanc,NULL,screen,&position);
+                    altern_color=0;
+                }
+                else
+                {
+                    //SDL_FillRect(noir,NULL,SDL_MapRGB(recta->format,0,0,0));
+                    SDL_BlitSurface(noir,NULL,screen,&position);
+                    altern_color=1;
+                }
+            }
+            altern_color--;
+        }
+}
+
+void draw_pions_plat()
+{
+    for(int j=0;j<8;j++)
+    {
+        for(int i=0;i<8;i++)
+        {
+            position.x=60+(62*i);
+            position.y=60+(62*j);
+
+            switch(pion[i][j])
+            {
+                case 1:
+                    //SDL_FillRect(pion_j,NULL,SDL_MapRGB(pion_j->format,0,255,255));
+                    SDL_BlitSurface(pion_j,NULL,screen,&position);
+                    break;
+
+                case 2:
+                    //SDL_FillRect(pion_n,NULL,SDL_MapRGB(pion_n->format,255,255,255));
+                    SDL_BlitSurface(pion_n,NULL,screen,&position);
+                    break;
+                case 3:
+                    //SDL_FillRect(pion_r,NULL,SDL_MapRGB(pion_r->format,255,0,0));
+                    SDL_BlitSurface(pion_r,NULL,screen,&position);
+                    break;
+            }
+        }
+    }
+}
+
+void event_SDL(SDL_Event event)
+{
+    SDL_WaitEvent(&event);
+    switch(event.type)
+    {
+        case SDL_QUIT:
+            continuer=0;
+            break;
+        case SDL_MOUSEBUTTONDOWN:
+            clic=true;
+            break;
+        case SDL_MOUSEBUTTONUP:
+            clic=false;
+            break;
+        case SDL_MOUSEMOTION:
+            x = event.motion.x;
+            y = event.motion.y;
+            break;
+        case SDL_KEYDOWN:
+            random_plateau();
+            break;
+    }
+}
+
+
+// TODO : reorganiser
+void random_plateau()
+{
+    srand(time(NULL));
+
+    int jaune = 34;
+    int rouge = 20;
+    int noir = 10;
+
+    int result;
+
+    for(int j=0;j<8;j++)
+    {
+        for(int i=0;i<8;i++)
+        {
+            result=(rand()%(6-1) +1);
+
+            cout << "result : " << result << endl;
+
+            if(result==1 || result==5 ||jaune>0 || result==6)
+            {
+                cout << "here" << endl;
+                jaune--;
+                cout << "j :"<< jaune << endl;
+                pion[i][j]=1;
+            }
+            else {result=3;}
+
+            if(result==3)
+            {
+                if(rouge>0)
+                {
+                    rouge--;
+                    cout << "r :"<< rouge << endl;
+                    pion[i][j]=3;
+                }
+                else
+                {
+                        result--;
+                }
+            }
+            if(result==2)
+            {
+                if(noir>0)
+                {
+                    noir--;
+                    cout << "n :"<< noir << endl;
+                    pion[i][j]=2;
+                }
+                else
+                {
+                    result--;
+                }
+            }
+            if(result==1)
+            {
+                    pion[i][j]=1;
+            }
+        }
+    }
+}
+
+void tour()
+{
+    player++;
+    if(player>2)
+    {
+        player=1;
+    }
+}
+
+void draw_pion(int x, int y, int color)
+{
+    position.x=x;
+    position.y=y;
+    switch(color)
+    {
+        case 1:
+            SDL_BlitSurface(pion_j,NULL,screen,&position);
+            break;
+        case 2:
+            SDL_BlitSurface(pion_n,NULL,screen,&position);
+            break;
+        case 3:
+            SDL_BlitSurface(pion_r,NULL,screen,&position);
+            break;
+    }
+}
